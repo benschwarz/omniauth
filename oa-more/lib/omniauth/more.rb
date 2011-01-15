ae9b11a259c005bfa1a94d12f16ce0a4f2213105
Paul Chilton <paul.chilton@gmail.com>
2011-01-16 04:36:11 +0800

    Flickr strategy for oa-more.

--- oa-more/lib/omniauth/more.rb
+++ oa-more/lib/omniauth/more.rb
@@ -3,5 +3,6 @@ require 'omniauth/core'
 module OmniAuth
   module Strategies
     autoload :WindowsLive, 'omniauth/strategies/windows_live'
+    autoload :Flickr, 'omniauth/strategies/flickr'
   end
 end
