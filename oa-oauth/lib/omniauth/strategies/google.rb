06c1e8bb1c5d12f9b2b22a0f713f8c714f86e553
Ben Schwarz <ben.schwarz@gmail.com>
2011-01-16 21:02:13 +1100

    Added google's sites as possible scopes for oauth requests

--- oa-oauth/lib/omniauth/strategies/google.rb
+++ oa-oauth/lib/omniauth/strategies/google.rb
@@ -12,6 +12,35 @@ module OmniAuth
     #    use OmniAuth::Strategies::Google, 'consumerkey', 'consumersecret'
     #
     class Google < OmniAuth::Strategies::OAuth
+      SCOPES = {
+        :analytics        => "https://www.google.com/analytics/feeds/",
+        :base             => "https://www.google.com/base/feeds/",
+        :buzz             => "https://www.googleapis.com/auth/buzz",
+        :book_search      => "https://www.google.com/books/feeds/",
+        :blogger          => "https://www.blogger.com/feeds/",
+        :calendar         => "https://www.google.com/calendar/feeds/",
+        :contacts         => "http://www.google.com/m8/feeds",
+        :chrome_web_store => "https://www.googleapis.com/auth/chromewebstore.readonly",
+        :docs             => "https://docs.google.com/feeds/",
+        :finance          => "https://finance.google.com/finance/feeds/",
+        :gmail            => "https://mail.google.com/mail/feed/atom",
+        :health           => "https://www.google.com/health/feeds/",
+        :h9               => "https://www.google.com/h9/feeds/",
+        :maps             => "https://maps.google.com/maps/feeds/",
+        :moderator        => "https://www.googleapis.com/auth/moderator",
+        :opensocial       => "https://www-opensocial.googleusercontent.com/api/people/",
+        :orkut            => "https://orkut.gmodules.com/social/rest",
+        :picasa           => "https://picasaweb.google.com/data/",
+        :sidewiki         => "https://www.google.com/sidewiki/feeds/",
+        :sites            => "https://sites.google.com/feeds/",
+        :spreadsheets     => "https://spreadsheets.google.com/feeds/",
+        :url_shortener    => "https://www.googleapis.com/auth/urlshortener",
+        :wave             => "http://wave.googleusercontent.com/api/rpc",
+        :webmaster_tools  => "https://www.google.com/webmasters/tools/feeds/",
+        :youtube          => "https://gdata.youtube.com",
+        :reader           => "https://www.google.com/reader/api/"
+      }
+      
       def initialize(app, consumer_key = nil, consumer_secret = nil, options = {}, &block)
         client_options = {
           :site => 'https://www.google.com',
@@ -19,6 +48,8 @@ module OmniAuth
           :access_token_path => '/accounts/OAuthGetAccessToken',
           :authorize_path => '/accounts/OAuthAuthorizeToken'
         }
+        
+        options[:scope] = :contacts unless options.key? :scope
 
         super(app, :google, consumer_key, consumer_secret, client_options, options)
       end
@@ -58,7 +89,7 @@ module OmniAuth
 
       # Monkeypatch OmniAuth to pass the scope in the consumer.get_request_token call
       def request_phase
-        request_token = consumer.get_request_token({:oauth_callback => callback_url}, {:scope => "http://www.google.com/m8/feeds"})
+        request_token = consumer.get_request_token({:oauth_callback => callback_url}, {:scope => SCOPE[options[:scope]]})
 
         (session['oauth']||={})[name.to_s] = {'callback_confirmed' => request_token.callback_confirmed?, 'request_token' => request_token.token, 'request_secret' => request_token.secret}
         r = Rack::Response.new
